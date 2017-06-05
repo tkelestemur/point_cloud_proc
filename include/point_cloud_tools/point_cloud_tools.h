3b980906e8adcf35769f92560035d34cd2f80dc0
Tarik Kelestemur <tkelestemur@gmail.com>
2017-06-05 16:38:53 -0400

    added functions

--- include/point_cloud_tools/point_cloud_tools.h
+++ include/point_cloud_tools/point_cloud_tools.h
@@ -1,16 +1,42 @@
+#ifndef POINTCLOUDTOOLS_H
+#define POINTCLOUDTOOLS_H
 
+#include "ros/ros.h"
+#include <sensor_msgs/PointCloud2.h>
+#include <tf/transform_listener.h>
+
+#include <pcl/point_types.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl_ros/transforms.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/voxel_grid.h>
 
 class PointCloudTools{
 
-typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloud;
+typedef pcl::PointCloud<pcl::PointXYZRGB> CloudT;
 typedef pcl::PointXYZRGB PointT;
 
 public:
-  PointCloudTools(ros::NodeHandle nh){
-
-
-  }
+  PointCloudTools();
+  void pointCloudCb(const sensor_msgs::PointCloud2 point_cloud);
+  bool segmentPlane();
 private:
-  PointCloud::Ptr cloud_raw_ (new PointCloud);
+
+  pcl::PassThrough<PointT> pass;
+  pcl::VoxelGrid<PointT> vg;
+  CloudT::Ptr cloud_raw_ (new CloudT);
+
+  ros::NodeHandle node_;
+  tf::TransformListener listener_;
+  ros::Subscriber point_cloud_sub_;
+  sensor_msgs::PointCloud2 raw_cloud_msg_;
+
+  std::string fixed_frame_;
+
+
 
 };
+
+#endif
--- src/point_cloud_tools.cpp
+++ src/point_cloud_tools.cpp
@@ -1,10 +1,33 @@
-// ros libraries
-#include "ros/ros.h"
+#include <point_cloud_tools/point_cloud_tools.h>
 
 
-int main(int argc, char** argv) {
+PointCloudTools::PointCloudTools()
+{
+
+  std::string point_cloud2_topic = "/camera/point_cloud";
+  node_.getParam("/point_cloud2_topic", point_cloud2_topic);
+  point_cloud_sub_ =  node_.subscribe<sensor_msgs::PointCloud2>(point_cloud2_topic,
+                                                                10,
+                                                                &PointCloudTools::pointCloudCb,
+                                                                this);
 
 
+}
 
-  return 0;
+bool PointCloudTools::segmentPlane(){
+// Transform point cloud to fixed frame
+fromROSMsg(raw_cloud_msg_, *cloud_raw_);
+fixed_frame_ = "base_link";
+CloudT::Ptr cloud_transformed(new CloudT);
+if (!pcl_ros::transformPointCloud(fixed_frame_, *cloud_raw_, *cloud_transformed, listener_))
+{
+  ROS_ERROR("Error transforming to frame %s", fixed_frame_.c_str());
+  return;
+}
+
+}
+
+
+void PointCloudTools::pointCloudCb(const sensor_msgs::PointCloud2 point_cloud){
+  raw_cloud_msg_ = point_cloud;
 }
