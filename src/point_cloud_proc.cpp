2fe66001f75041302f45c4d1d0c8cb5c0930ab9d
Tarik Kelestemur <tkelestemur@gmail.com>
2017-07-05 17:59:41 -0400

    updates

--- src/point_cloud_proc.cpp
+++ src/point_cloud_proc.cpp
@@ -5,10 +5,16 @@
 #include <tf/transform_listener.h>
 #include <geometry_msgs/PolygonStamped.h>
 #include <gpd/CloudIndexed.h>
+#include <shape_msgs/Plane.h>
+#include <shape_msgs/Mesh.h>
+#include <object_msgs/PlaneObjects.h>
+#include <geometric_shapes/body_operations.h>
+#include <geometric_shapes/shape_operations.h>
 
 // PCL
 #include <pcl_ros/point_cloud.h>
 #include <pcl_ros/transforms.h>
+#include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/passthrough.h>
 #include <pcl/filters/voxel_grid.h>
@@ -26,6 +32,7 @@
 
 // Other
 #include <boost/thread/mutex.hpp>
+#include <boost/scoped_ptr.hpp>
 #include <Eigen/Dense>
 
 class PointCloudProc{
@@ -56,11 +63,12 @@ class PointCloudProc{
       point_cloud_sub_ = nh_.subscribe<CloudT> (point_cloud_topic_, 10, &PointCloudProc::pointCloudCb, this);
       table_cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("table_cloud", 10);
       object_cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("object_cloud", 10);
-      plane_polygon_pub_ = nh_.advertise<geometry_msgs::PolygonStamped>("plane_polygon", 10);
+      plane_object_pub_ = nh_.advertise<object_msgs::PlaneObjects>("plane_objects", 10);
       gpd_cloud_pub_ = nh_.advertise<gpd::CloudIndexed>("indexed_cloud", 10);
       object_cluster_srv_ = nh_.advertiseService ("extract_objects", &PointCloudProc::objectClusterServiceCb, this);
       segment_multiple_plane_srv_ = nh_.advertiseService ("segment_multiple_plane", &PointCloudProc::segmentMultiplePlaneServiceCb, this);
       create_gpd_msg_srv_ = nh_.advertiseService ("create_gpd_msg", &PointCloudProc::createGPDMsgServiceCb, this);
+      find_table_srv_ = nh_.advertiseService ("find_table", &PointCloudProc::findTableServiceCb, this);
 
     }
 
@@ -75,11 +83,10 @@ class PointCloudProc{
       std::string fixed_frame = "/base_link"; // TODO: Make it rosparam
       bool transform_success = pcl_ros::transformPointCloud(fixed_frame, *cloud_raw_, *cloud_transformed, listener_);
       cloud_transformed_ = cloud_transformed;
-      std::cout << "transformed cloud height: " <<  cloud_transformed_->height << '\n';
       return transform_success;
     }
 
-    bool filterPointCloud(){
+    bool filterPointCloud(bool use_voxel=true){
 
       CloudT::Ptr cloud_filtered (new CloudT);
       CloudT::Ptr cloud_filtered_2 (new CloudT);
@@ -99,12 +106,14 @@ class PointCloudProc{
       pass_.filter(*cloud_filtered);
 
       // Downsample point cloud
-      vg_.setInputCloud (cloud_filtered);
-      vg_.setLeafSize (leaf_size_, leaf_size_, leaf_size_);
-      vg_.filter (*cloud_filtered_2);
+      if (use_voxel) {
+        vg_.setInputCloud (cloud_filtered);
+        vg_.setLeafSize (leaf_size_, leaf_size_, leaf_size_);
+        vg_.filter (*cloud_filtered_2);
+        cloud_filtered_ = cloud_filtered_2;
+      } else cloud_filtered_ = cloud_filtered;
 
-      cloud_filtered_ = cloud_filtered_2;
-      std::cout << "cloud height: " <<  cloud_filtered_->height << '\n';
+
       ROS_INFO("Point cloud is filtered!");
       if (cloud_filtered_->points.size() == 0) {
         ROS_WARN("Point cloud is empty after filtering!");
@@ -114,7 +123,7 @@ class PointCloudProc{
       return true;
     }
 
-    void segmentSinglePlane(){
+    bool segmentSinglePlane(){
 
       CloudT::Ptr cloud_plane (new CloudT);
       CloudT::Ptr cloud_hull (new CloudT);
@@ -124,13 +133,18 @@ class PointCloudProc{
 
       Eigen::Vector3f axis = Eigen::Vector3f(0.0,0.0,1.0); //z axis
       seg_.setOptimizeCoefficients (true);
-      seg_.setModelType (pcl::SACMODEL_PLANE);
+      seg_.setModelType (pcl::SACMODEL_PERPENDICULAR_PLANE);
       seg_.setMethodType (pcl::SAC_RANSAC);
       seg_.setAxis(axis);
-      seg_.setEpsAngle(30.0f * (M_PI/180.0f));
+      seg_.setEpsAngle(20.0f * (M_PI/180.0f));
       seg_.setDistanceThreshold (0.01);
       seg_.setInputCloud (cloud_filtered_);
       seg_.segment (*inliers, *coefficients);
+
+      if (inliers->indices.size() == 0) {
+        ROS_INFO("No plane found!");
+        return false;
+      }
 
       ROS_INFO("Single plane is segmented!");
       ROS_INFO_STREAM("Coefficients: " << coefficients->values[0] << " "
@@ -142,6 +156,7 @@ class PointCloudProc{
       extract_.setNegative(false);
       extract_.setIndices (inliers);
       extract_.filter (*cloud_plane);
+      ROS_INFO_STREAM("# of points in plane: " << cloud_plane->points.size());
 
       if (debug_) {
         table_cloud_pub_.publish(cloud_plane);
@@ -153,21 +168,43 @@ class PointCloudProc{
 
       cloud_hull_ = cloud_hull;
 
-      // Publish boundary of the table
-      geometry_msgs::PolygonStamped plane_polygon;
+      Eigen::Vector4f center;
+      pcl::compute3DCentroid(*cloud_plane, center);
+
+
+      // Construct plane object msg
+      object_msgs::PlaneObjects plane_object_msgs;
+      object_msgs::PlaneObject plane_object_msg;
+
+      pcl_conversions::fromPCL(cloud_plane->header, plane_object_msg.header);
+
+      // Get plane center
+      plane_object_msg.center.x = center[0];
+      plane_object_msg.center.y = center[1];
+      plane_object_msg.center.z = center[2];
+
+      // Get plane polygon
       for (int i = 0; i < cloud_hull->points.size (); i++) {
-        geometry_msgs::Point32 pt;
+        geometry_msgs::Point pt;
         pt.x = cloud_hull->points[i].x;
         pt.y = cloud_hull->points[i].y;
         pt.z = cloud_hull->points[i].z;
 
-        plane_polygon.polygon.points.push_back(pt);
+        plane_object_msg.polygon.push_back(pt);
       }
-      plane_polygon.header.frame_id = cloud_hull->header.frame_id;
-      plane_polygon.header.stamp = ros::Time::now(); //TODO: should this be point cloud time?
 
-      plane_polygon_pub_.publish(plane_polygon);
+      // Get plane coefficients
+      plane_object_msg.coef[0] = coefficients->values[0];
+      plane_object_msg.coef[1] = coefficients->values[1];
+      plane_object_msg.coef[2] = coefficients->values[2];
+      plane_object_msg.coef[3] = coefficients->values[3];
 
+      plane_object_msg.size.data = cloud_plane->points.size();
+
+      plane_object_msgs.plane_objects.push_back(plane_object_msg);
+      plane_object_pub_.publish(plane_object_msgs);
+
+      return true;
     }
 
 
@@ -175,6 +212,13 @@ class PointCloudProc{
 
       pcl::PointCloud<pcl::Normal>::Ptr normal_cloud (new pcl::PointCloud<pcl::Normal>);
       std::vector<pcl::PlanarRegion<PointT>, Eigen::aligned_allocator<pcl::PlanarRegion<PointT> > > regions;
+      std::vector<pcl::ModelCoefficients> plane_coefficients;
+      std::vector<pcl::PointIndices> plane_indices;
+      std::vector<pcl::PointIndices> label_indices;
+      std::vector<pcl::PointIndices> boundary_indices;
+      std::vector<Eigen::Vector4f, Eigen::aligned_allocator< Eigen::Vector4f > > centroids;
+      std::vector<Eigen::Matrix3f, Eigen::aligned_allocator< Eigen::Matrix3f > > covariances;
+      pcl::PointCloud<pcl::Label>::Ptr labels (new pcl::PointCloud<pcl::Label>);
 
       pcl::IntegralImageNormalEstimation<PointT, pcl::Normal> ne;
       ne.setNormalEstimationMethod (ne.COVARIANCE_MATRIX);
@@ -183,26 +227,68 @@ class PointCloudProc{
       ne.setInputCloud (cloud_transformed_);
       ne.compute (*normal_cloud);
 
-
       pcl::OrganizedMultiPlaneSegmentation<PointT, pcl::Normal, pcl::Label> mps;
-      mps.setMinInliers (200);
-      mps.setAngularThreshold (0.017453 * 2.0); //3 degrees
+      mps.setMinInliers (700);
+      mps.setAngularThreshold ((M_PI/180) * 3.0); //3 degrees
       mps.setDistanceThreshold (0.02); //2cm
       mps.setInputNormals (normal_cloud);
       mps.setInputCloud (cloud_transformed_);
-      mps.segmentAndRefine (regions);
+      mps.segmentAndRefine (regions, plane_coefficients, plane_indices, labels, label_indices, boundary_indices);
 
-      if (regions.size() == 0) {
+      if (regions.size() != 0) {
+        ROS_INFO_STREAM("Number of planes: " << regions.size());
+      } else {
         ROS_INFO("No plane found!");
+        return;
       }
 
-      for (size_t i = 0; i < regions.size (); i++){
+      // Construct plane object msg
+      object_msgs::PlaneObjects plane_object_msgs;
+      object_msgs::PlaneObject plane_object_msg;
+
+      for (int i = 0; i < regions.size (); i++){
+
+        pcl_conversions::fromPCL(cloud_transformed_->header, plane_object_msg.header);
+
+        // Get plane coefficients
         Eigen::Vector4f model = regions[i].getCoefficients();
-        ROS_INFO_STREAM("Plane "<< i+1 << " coefficients: " << model[0] << " "
-                                                          << model[1] << " "
-                                                          << model[2] << " "
-                                                          << model[3]);
+        plane_object_msg.coef[0] = model[0];
+        plane_object_msg.coef[1] = model[1];
+        plane_object_msg.coef[2] = model[2];
+        plane_object_msg.coef[3] = model[3];
+
+        // Get plane center
+        Eigen::Vector3f centroid = regions[i].getCentroid();
+        plane_object_msg.center.x = centroid[0];
+        plane_object_msg.center.y = centroid[1];
+        plane_object_msg.center.z = centroid[2];
+
+        // Get plane polygon
+        pcl::PointCloud<PointT>::Ptr contour (new pcl::PointCloud<PointT>);
+        contour->points = regions[i].getContour();
+
+        // std::cout << "number of points in contour: " << contour->points.size() << '\n';
+        // std::cout << "number of points in plane_indices: " << plane_indices[i].indices.size() << '\n';
+        // std::cout << "number of points in boundary_indices: " << boundary_indices[i].indices.size() << '\n';
+        // std::cout << "number of points in label_indices: " << label_indices[i].indices.size() << '\n';
+
+        for (int j = 0; j < contour->points.size (); j++) { // TODO: Find a function that does this job.
+          geometry_msgs::Point pt;
+          pt.x = contour->points[j].x;
+          pt.y = contour->points[j].y;
+          pt.z = contour->points[j].z;
+
+          plane_object_msg.polygon.push_back(pt);
+        }
+
+        // Get point cloud size
+        plane_object_msg.size.data = plane_indices[i].indices.size();
+
+        plane_object_msgs.plane_objects.push_back(plane_object_msg);
+        plane_object_pub_.publish(plane_object_msgs);
+
       }
+
 
     }
 
@@ -304,6 +390,25 @@ class PointCloudProc{
 
   }
 
+  bool findTableServiceCb(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res){
+    if (!PointCloudProc::transformPointCloud()){
+      ROS_INFO("Couldn't transform point cloud!");
+      return false;
+    }
+
+    if (!PointCloudProc::filterPointCloud(false)){
+      ROS_INFO("Couldn't filter point cloud!");
+      return false;
+    }
+
+    if (!PointCloudProc::segmentSinglePlane()){
+      ROS_INFO("Couldn't segment a plane!");
+      return false;
+    }
+
+    return true;
+  }
+
   private:
     pcl::PassThrough<PointT> pass_;
     pcl::VoxelGrid<PointT> vg_;
@@ -327,9 +432,9 @@ class PointCloudProc{
     ros::NodeHandle nh_;
     ros::Subscriber point_cloud_sub_;
     ros::Publisher table_cloud_pub_, object_cloud_pub_, gpd_cloud_pub_;
-    ros::Publisher plane_polygon_pub_;
+    ros::Publisher plane_object_pub_;
     ros::ServiceServer object_cluster_srv_, create_gpd_msg_srv_;
-    ros::ServiceServer segment_multiple_plane_srv_;
+    ros::ServiceServer segment_multiple_plane_srv_, find_table_srv_;
     tf::TransformListener listener_;
 
 
